7145c4ed178bc9c6c55018f0e0a52725eda9ea3e
Eddy Ramos <eddyinf605@gmail.com>
2022-10-11 23:53:53 -0400

    se hizo unas modificaciones al estilo

--- index.php
+++ index.php
@@ -5,13 +5,17 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
 	<title>Formulario</title>
 </head>
 
 <body>
 	<div class="contenedor">
 		<form action="validacion.php" method="post">
-			<h2>Login</h2>
+			<div class="titulo">
+				<h2>Login</h2>
+			</div>
+
 			<br>
 			<label for="Correo">Correo</label>
 			<br>
