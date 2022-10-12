fcb934ffa45d697a8832e2b244e07ec88d19537a
Eddy Ramos <eddyinf605@gmail.com>
2022-10-11 23:03:37 -0400

    subiendo el proyecto al repositorio

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Formulario</title>
+</head>
+
+<body>
+	<div class="contenedor">
+		<form action="" method="post">
+			<h2>Login</h2>
+			<br>
+			<label for="Correo">Correo</label>
+			<br>
+			<input type="email" name="email" id="">
+			<br>
+			<br>
+			<label for="">Contraseña</label>
+			<br>
+			<input type="password" name="password" id="">
+			<br>
+			<br>
+			<button>Iniciar Seción</button>
+		</form>
+
+	</div>
+
+</body>
+
+</html>
